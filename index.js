f62c804591949b773bd14bc5b90773571f688e04
MariiaKpln <kpln.mary@gmail.com>
2025-03-02 14:03:03 +0200

    it works

--- index.js
+++ index.js
@@ -17,14 +17,6 @@ expectedResults = [
   [1, 3],
 ];
 
-// console.log(
-//   test({
-//     script: 'minMax(["hello", "kuku", "abc"])',
-//     expected: ["abc", "kuku"],
-//   })
-// );
-// console.log(testframework({ script: "scripts", expected: expectedResults }));
-
 function myReduce(array, callback, initialValue) {
   let acc = initialValue ?? array[0];
   const index = initialValue == undefined ? 1 : 0;
@@ -79,17 +71,17 @@ function createTestResult(script, expectedJSON, actualJSON, result) {
 }
 
 function testframework(scripts, expectedResults) {
-  let testObj = makeObj(scripts, expectedResults);
-  let result = createArray(testObj);
-  console.log("whattt", result); //
   let passedTests = 0;
   let failedTests = 0;
-  const bodyElem = document.querySelector("body");
-  // result.map((el) => {
-  //   let isPassed = el.result;
-  //   isPassed === "passed" ? "green" && passedTests++ : "red" && failedTests++;
-  // });
+  let testObj = makeObj(scripts, expectedResults);
+  let result = createArray(testObj);
+  result.map((el) => (el.result === "passed" ? passedTests++ : failedTests++));
+  const bodyElement = document.querySelector("body");
+  bodyElement.innerHTML = `${orderedList(result)}
+  <p class="item green">Passed Tests: ${passedTests}</p>
+<p class="item">Failed Tests: ${failedTests}</p>`;
 
+  return result;
   //TODO
   //input
   //scripts - array of tested scripts
@@ -97,29 +89,6 @@ function testframework(scripts, expectedResults) {
   //scrpits[i] and expectedResults[i] should be consistent
   /**************************************************************** */
   //output
-
-  // const bodyElem = document.querySelector("body");
-  bodyElem.innerHTML = `
-  <ol>
-    ${result
-      .map(
-        (testResult) => `
-      <li style="color: ${
-        testResult.result === "passed"
-          ? "green" && passedTests++
-          : "red" && failedTests++
-      };">
-        <strong>Script:</strong> ${testResult.script} 
-        <br><strong>Expected:</strong> ${testResult.expectedJSON} 
-        <br><strong>Actual:</strong> ${testResult.actualJSON}
-      </li>
-    `
-      )
-      .join("")}
-  </ol>
-  <p style="color: green;">Passed Tests: ${passedTests}</p>
-  <p style="color: red;">Failed Tests: ${failedTests}</p>
-`;
   //bodyElem.innerHTML = <orderedList of test results with coloring legend: passed tests by green,
   //  failed tests by red. After list summary including number of passed tests and number of failed tests with
   //appropriate coloring (green /red)
@@ -141,12 +110,6 @@ function makeObj(scripts, expectedResults) {
 function createArray(testObj) {
   let result = Object.values(testObj).reduce((acc, obj) => {
     const actual = test(obj); // Получаем результат выполнения скрипта
-    // const isPassed = actual.result;
-    // console.log("actual", actual);
-    // Обновляем счетчики
-    // isPassed === "passed" ? passedTests++ : failedTests++;
-
-    // Добавляем результат теста в аккумулятор
     acc.push(actual);
 
     return acc;
@@ -154,4 +117,16 @@ function createArray(testObj) {
   return result;
 }
 
+function orderedList(array) {
+  let items = array.map((el) => makeItems(el));
+  return `<ul>${items}</ul>`;
+}
 
+function makeItems(el) {
+  let res = `<li class="item">${el.script}</li><li class="item">${el.expectedJSON}</li><li class="item">${el.actualJSON}</li>`;
+
+  if (el.result === "passed") {
+    res = `<li class="item green">${el.script}</li><li class="item green">${el.expectedJSON}</li><li class="item green">${el.actualJSON}</li>`;
+  }
+  return res;
+}
